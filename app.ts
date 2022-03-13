d809f80a69c94b6f19b7e67dab3639df52959dd5
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-03-13 14:28:05 +0100

    :BUILD: => start projet backend

---
+++ app.ts
@@ -0,0 +1,9 @@
+
+
+const express      = require('express');
+const app           = express();
+const helmet        = require('helmet');  
+const mongoose      = require('mongoose');
+
+const environnement = require('dotenv');       
+      environnement.config();
