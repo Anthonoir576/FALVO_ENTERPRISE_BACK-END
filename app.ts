5205b11b8ba005681c8c8029178a4b99d23bd3b0
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-03-19 13:24:46 +0100

    :BUG: => probleme de mise en place serveur

--- app.ts
+++ app.ts
@@ -4,6 +4,33 @@ const express       = require('express');
 const app           = express();
 const helmet        = require('helmet');  
 const mongoose      = require('mongoose');
+const cors          = require('cors');
 
 const environnement = require('dotenv');       
       environnement.config();
+
+
+
+
+
+const corsControls: any = {
+      origin             : '*',
+      credentials        : true,
+      'allowedHeaders'   : ['sessionId', 'Content-Type', 'Origin', 'X-Requested-With', 'X-Auth-Token', 'Content', 'Accept', 'Authorization' ],
+      'exposedHeaders'   : ['sessionId'],
+      'methods'          : 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'preflightContinue': false
+};
+      
+app.use(cors(corsControls));
+
+
+app.use(helmet());
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+module.exports = app;
--- server.ts
+++ server.ts
@@ -1,1 +1,22 @@
 
+/* ##########   MES DECLARATIONS   ################ */
+const http :any         = require('http');               
+require('app');      
+
+require('dotenv')
+    .config({ path: './config/.env' });               
+
+/* ################################################ */
+
+
+
+/* ################  SERVEUR  ##################### */
+app.set('port', 4000);           
+
+const server = http.createServer(app);               
+server.listen( 4000, () => {      
+
+    console.log(`Le serveur fonctionne sur le port ici`);
+
+});
+/* ################################################ */
