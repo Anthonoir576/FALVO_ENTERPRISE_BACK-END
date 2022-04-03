deb8bbcea0ca330ba9d1b3fcd9be27bddb7a95d8
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-04-03 14:22:30 +0200

    :FEAT: => structure regex

--- src/security/regex.ts
+++ src/security/regex.ts
@@ -1,17 +1,17 @@
 
 
-
 module.exports = (request? :any, response? :any, next? : any) => {   
 
 
-    let email        = new RegExp('^[a-zA-Z0-9._-]+[@]+[a-zA-Z0-9.-_]+[.]+[a-z]{2,5}[ ]{0,2}$', 'g');
-    let testEmail    = email.test(request.body.email.trim());
-    let password     = new RegExp(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){4,16}$/gm);
-    let testPassword = password.test(request.body.password.trim());
+    let reqMail       = request.body.email.trim();
+    let reqPassword   = request.body.password.trim();
+    let email         = new RegExp('^[a-zA-Z0-9._-]+[@]+[a-zA-Z0-9.-_]+[.]+[a-z]{2,5}[ ]{0,2}$', 'g');
+    let testEmail     = email.test(reqMail);
+    let password      = new RegExp(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){4,16}$/gm);
+    let testPassword  = password.test(reqPassword);
 
 
-    try {
-        
+    try {     
         if (testEmail && testPassword) {
             next();
         } else if (!testEmail) {
@@ -19,11 +19,10 @@ module.exports = (request? :any, response? :any, next? : any) => {
         } else if (!testPassword) {
             response.status(200).json({ message : 'ERREUR : Le mot de passe doit contenir au minimum 4 caractères, maximum 16 caractères, une majuscule, une minuscule, et un caractère spéciaux'});
         };
-
     } catch (error) {                
-       
         return error; 
-
     };
 
+
+    
 };
