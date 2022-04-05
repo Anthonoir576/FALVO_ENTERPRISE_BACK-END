8c33f80a9489ca175ec9d008e8e0c9b319e8b5cc
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-04-05 06:11:49 +0200

    :FEAT: => Petit maj sur la regex de password a mdp comme le schema

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,7 +1,7 @@
 const expressUser       = require('express');                       
 const routerUser        = expressUser.Router();                          
 const usersCtrl         = require('../controllers/userControllers');
-
+const regexp            = require('../security/regex');
 
 routerUser.post('/signup', usersCtrl.signup);
 
